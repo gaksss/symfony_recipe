2e68442732f3f033e5822069592fbd0849deecf3
gaksss <clement.carlier2@gmail.com>
2025-01-31 16:43:25 +0100

    first commit

---
+++ src/Controller/Admin/RecipeCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Recipe;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class RecipeCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Recipe::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            
+            TextField::new('name'),
+            TextEditorField::new('description'),
+            DateTimeField::new('createdAt'),
+            AssociationField::new('category')->autocomplete(),
+        ];
+    }
+    
+}
